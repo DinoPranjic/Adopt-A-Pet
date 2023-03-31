bd4e1fbcc3bf3dc05e490dcc0fc2ab7d3b8c44ad
Dino <dinopranjic92@gmail.com>
2023-03-31 11:45:09 -0400

    Update .env variables

--- src/utils/fetchToken.ts
+++ src/utils/fetchToken.ts
@@ -1,10 +1,8 @@
-import { CLIENT_ID, CLIENT_SECRET } from "../../secrets";
-
 const fetchToken = async (url: string) => {
   const params = new URLSearchParams();
   params.append("grant_type", "client_credentials");
-  params.append("client_id", CLIENT_ID);
-  params.append("client_secret", CLIENT_SECRET);
+  params.append("client_id", process.env.REACT_APP_API_KEY as string);
+  params.append("client_secret", process.env.REACT_APP_SECRET as string);
 
   const token = await fetch(url, {
     method: "POST",
@@ -15,7 +13,7 @@ const fetchToken = async (url: string) => {
   }).catch((err) => {
     console.log(err);
   })
-
+  console.log(process.env);
   return token;
 }
 

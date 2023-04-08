9a56841b333cf8a19328e4b29a4103d2235b8515
Dino <dinopranjic92@gmail.com>
2023-04-08 10:59:19 -0400

    Create Pet type

---
+++ src/context/petTypes.ts
@@ -0,0 +1,11 @@
+export type Pet = {
+  name: string,
+  age: string,
+  gender: string,
+  species: string,
+  size: string,
+  url: string,
+  photoURL: string,
+  email: string,
+  phone: string
+};

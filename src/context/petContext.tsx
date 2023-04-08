b94bc0d088071a87353ba87016532761b00ded9b
Dino <dinopranjic92@gmail.com>
2023-04-08 11:12:26 -0400

    Add pet context provider

--- src/context/petContext.tsx
+++ src/context/petContext.tsx
@@ -1,1 +1,39 @@
-import { Dispatch, SetStateAction } from "react";
+import { useState, useContext, createContext, ReactNode } from 'react';
+
+import { IPetContext, Pet } from './petTypes';
+
+const PetContext = createContext<IPetContext | null>(null);
+
+const usePetContext = () => {
+  const context = useContext(PetContext);
+
+  if (!context) {
+    throw new Error('No Context Provided!');
+  }
+
+  return context;
+
+};
+
+const PetProvider = ({ children }: { children?: ReactNode}) => {
+  const [pets, setPets] = useState<Pet[] | null>(null);
+
+  const petState = {
+    pets
+  };
+
+  const petActions = {
+    setPets
+  };
+
+  return(
+    <PetContext.Provider
+    value={{
+      petState,
+      petActions
+    }}
+    >
+      {children}
+    </PetContext.Provider>
+  )
+}
